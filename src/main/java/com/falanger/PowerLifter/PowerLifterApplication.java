eb1d44ed4a7aee2699e3c78b3eb881acfa0a5128
Falanger-debug <falanger27@gmail.com>
2024-05-28 11:38:28 +0200

    initial commit

---
+++ src/main/java/com/falanger/PowerLifter/PowerLifterApplication.java
@@ -0,0 +1,13 @@
+package com.falanger.PowerLifter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowerLifterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PowerLifterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/falanger/PowerLifter/PowerLifterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.falanger.PowerLifter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PowerLifterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
